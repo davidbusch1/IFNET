9dc7361938fd1dde973bd25aeead92bd8186e6d0
davidbusch1 <davidbusch123@gmail.com>
2021-04-14 21:06:16 -0300

    mudei os erros do inteliJ

--- src/com/company/ifnet/Professor.java
+++ src/com/company/ifnet/Professor.java
@@ -1,5 +1,7 @@
 package com.company.ifnet;
 
+import java.util.Scanner;
+
 public class Professor extends Usuario{
 
     private String area;
@@ -15,6 +17,31 @@ public class Professor extends Usuario{
 
     public void setArea(String area) {
         this.area = area;
+    }
+
+    public static Professor cadastrarProfessor() throws CadastroException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Nome do Professor: ");
+        String nomeProfessor = leitura.nextLine();
+        if (nomeProfessor.equals("")) {
+            throw new CadastroException("Você deve digitar um Nome!");
+        } else {
+            System.out.print("Prontuário: ");
+            String prontuarioProfessor = leitura.nextLine();
+            if (prontuarioProfessor.equals("")) {
+                throw new CadastroException("Você deve digitar um Prontuário!");
+            } else {
+                System.out.print("Área: ");
+                String areaProfessor = leitura.nextLine();
+                if (areaProfessor.equals("")) {
+                    throw new CadastroException("Você deve digitar uma Área!");
+                } else {
+                    Professor p = new Professor(nomeProfessor, prontuarioProfessor, areaProfessor);
+                    System.out.println("Professor cadastrado com sucesso!");
+                    return p;
+                }
+            }
+        }
     }
 
     @Override
